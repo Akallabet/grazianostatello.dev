9d7098519e1f07dd22ce1c3a6c846caac457e4e9
Graziano Statello <grazianos86@gmail.com>
2024-01-10 08:08:49 +0100

    Add title and avatar image

---
+++ app/routes/_index.tsx
@@ -0,0 +1,46 @@
+import type { MetaFunction } from "@remix-run/node";
+import avatar from "../images/avatar.jpeg";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Graziano Statello" },
+    { name: "description", content: "Personal website of Graziano Statello" },
+  ];
+};
+
+function Links() {
+  return "";
+}
+
+function Title() {
+  return <h1>Graziano Statello</h1>;
+}
+
+function Avatar() {
+  return (
+    <img
+      src={avatar}
+      alt="Graziano Statello"
+      className="rounded-full max-w-xs"
+    />
+  );
+}
+
+function Header() {
+  return (
+    <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-2 ">
+      <Title />
+      <Avatar />
+    </div>
+  );
+}
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <div className="max-w-screen-2xl mx-auto">
+        <Header />
+      </div>
+    </div>
+  );
+}
